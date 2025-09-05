6f36024a5f68a450dac1f0b5f2dab5fafb602346
tampv <tampv@ssg.vn>
2025-09-05 10:19:02 +0700

    #first commit

---
+++ DemoGitHubWeb/DemoGitHubWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DemoGitHubWeb.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
