c5d8b935487b23b713d03ae6a8654d016ab431af
tampv <tampv@ssg.vn>
2025-09-05 15:42:52 +0700

    Bo sung tinh nang XYZ

---
+++ DemoGitHubWeb/DemoGitHubWeb/Utilities/Helpers.cs
@@ -0,0 +1,16 @@
+﻿namespace DemoGitHubWeb.Utilities;
+
+public static class Key
+{
+    public static string AzureKey = "a1b2c3d4e5f64789abcdef1234567890";
+    
+    
+    
+    
+    public static string AzureStorageAccountKey = "1+exampleFakeAzureKey1234567890abcdefghijklmnoPQRSTuvwxYZabcdEFGH==";
+    
+    
+    
+    public static string AWS Keys = "1+exampleFakeAzureKey1234567890abcdefghijklmnoPQRSTuvwxYZabcdEFGH==";
+    public static string AWS Keys = "AWS Keys";
+}
