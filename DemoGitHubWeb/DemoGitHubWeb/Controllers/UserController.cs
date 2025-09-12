ce7f95f47c5bae0bc1559b6d37313e2e7a2cc23f
phavatam <phavatam@gmail.com>
2025-09-12 17:03:03 +0700

    #feat thêm API GetUser_Vulnerable

--- DemoGitHubWeb/DemoGitHubWeb/Controllers/UserController.cs
+++ DemoGitHubWeb/DemoGitHubWeb/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System.Data;
+using System.Diagnostics;
 using DemoGitHubWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,6 +14,24 @@ public class UserController: Controller
         _logger = logger;
     }
 
+    // Giả sử connectionString được lấy từ file config
+    private string connectionString = "Server=myServer;Database=myDataBase;User Id=myUser;Password=myPassword;";
+
+    /// <summary>
+    /// PHIÊN BẢN CÓ LỖ HỔNG BẢO MẬT
+    /// Lấy thông tin người dùng từ cơ sở dữ liệu bằng username.
+    /// Lỗ hổng: Xây dựng câu lệnh SQL bằng cách nối chuỗi trực tiếp từ input.
+    /// </summary>
+    public DataTable GetUser_Vulnerable(string username)
+    {
+        // **LỖ HỔNG NẰM Ở ĐÂY**
+        // Dữ liệu 'username' từ người dùng được nối thẳng vào câu lệnh SQL.
+        string query = "SELECT UserID, FirstName, LastName, Email FROM Users WHERE Username = '" + username + "'";
+
+        DataTable dataTable = new DataTable();
+        return dataTable;
+    }
+    
     public IActionResult UserList()
     {
         _logger.LogInformation("Testing logging in UserController Index action.");
