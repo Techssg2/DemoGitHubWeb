971c862e97e5d99c3d43f1228ceba322dfac7233
phavatam <phavatam@gmail.com>
2025-09-15 10:35:41 +0700

    #feat demo git ac

--- DemoGitHubWeb/DemoGitHubWeb/Controllers/HomeController.cs
+++ DemoGitHubWeb/DemoGitHubWeb/Controllers/HomeController.cs
@@ -28,6 +28,7 @@ public class HomeController : Controller
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
+        string name = "Admin'; DROP TABLE Users; --";
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
