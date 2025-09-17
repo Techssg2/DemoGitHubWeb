9fd0395f1d0aef0f44fd5455b3fee1d08bff8688
phavatam <phavatam@gmail.com>
2025-09-17 14:24:54 +0700

    #feat thêm key

--- DemoGitHubWeb/DemoGitHubWeb/Models/Const.cs
+++ DemoGitHubWeb/DemoGitHubWeb/Models/Const.cs
@@ -5,12 +5,12 @@ public class Const
     public static readonly string APIKey = "AIzaSyD-EXAMPLEKEY1234567890ABCDEFGHIJK";
 }
 
+public class GithubKey
+{
+    public const string FakeGitHubToken = "ghp_aBcDeFgHiJkLmNoPqRsTuVwXyZ1234567890";
+}
 public class StripeKey
 {    
     // Khóa API theo định dạng Stripe
     public const string FakeStripeApiKey = "sk_live_51H...[redacted_for_safety]...Afs2";
-}
-public class GithubKey
-{
-    public const string FakeGitHubToken = "ghp_aBcDeFgHiJkLmNoPqRsTuVwXyZ1234567890";
 }
