59e5d0768de63a58a190f5cc03838f9d1e21d89c
phavatam <phavatam@gmail.com>
2025-09-17 14:33:09 +0700

    #feat thêm key google

--- DemoGitHubWeb/DemoGitHubWeb/Models/Const.cs
+++ DemoGitHubWeb/DemoGitHubWeb/Models/Const.cs
@@ -4,11 +4,10 @@ public class Const
 {
     public static readonly string APIKey = "AIzaSyD-EXAMPLEKEY1234567890ABCDEFGHIJK";
 }
-
 public class GithubKey
 {
     public const string FakeGitHubToken = "ghp_aBcDeFgHiJkLmNoPqRsTuVwXyZ1234567890";
-}
+}   
 public class StripeKey
 {    
     // Khóa API theo định dạng Stripe
